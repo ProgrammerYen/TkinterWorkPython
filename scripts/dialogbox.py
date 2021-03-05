c1dc77b4ce7540da1d09f99dded79ec02559e3df
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2021-03-05 19:37:55 +0000

    Created a running code that allows the user to choose an imaging file. Afterward it output the path and displays the image.

--- scripts/dialogbox.py
+++ scripts/dialogbox.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import ImageTk, Image
 
 root = Tk()
@@ -9,7 +10,20 @@ root.iconbitmap("logos/food-pizza_115113.ico")
 
 root.filename = filedialog.askopenfilename(
     initialdir="C:/Users/DeAlwis_Consulting/OneDrive/Family/Yenula",
-    title = "Please select a file", filetypes=(("All Files", "*.*"), ("Plain Text", "*.txt"), ("png", "*.png"), ("jpg", "*.jpg"), ("ico", "*.ico"),
-                                               ("html", "*.html"), ("python", "*.py"), ("CSS", "*.css")))   
+    title = "Please select a file", filetypes=(("PNG Files", "*.png"), ("JPG Files", "*.jpg"), ("ICO Files", "*.ico")))
 
+fileLabel = Label(root, text=root.filename)
+fileLabel.grid(row=0, column=0)
+imageChosen = ImageTk.PhotoImage(Image.open(root.filename))
+
+imageLabel = Label(root, image=imageChosen)
+imageLabel.grid(row=1, column=0)
+
+def messageExit():
+    response = messagebox.askquestion("Verify the you wish to exit program", "Are you sure you wish to exit the program?")
+    if response == "yes":
+        root.destroy()
+
+buttonExit = Button(root, text="Exit Program", command=messageExit)
+buttonExit.grid(row=2, column=0)
 root.mainloop()
