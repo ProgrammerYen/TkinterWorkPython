5048a26722f93c07bc66efcebd15c1a852d96c91
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2021-04-05 14:18:19 +0100

    Made changes

--- scripts/databases.py
+++ scripts/databases.py
@@ -7,9 +7,26 @@ root.title("Databases")
 root.geometry("400x400")
 root.resizable(False, False)
 
-# Databases
-database_connect = sqlite3.connect("databases/address-book.db")
+# Create or connect to a database
+database_connect = sqlite3.connect("address-book.db")
 
+# Creating cursor
+cursor = database_connect.cursor()
+
+# Make table in database
+cursor.execute("""CREATE A TABLE addresses (
+    first_name text
+    last_name text
+    address text
+    city text
+    age integer
+    zipcode integer)""")
+
+# Commit changes
+database_connect.commit()
+
+# Close database connection
+database_connect.close()
 
 # Creating main event loop
 root.mainloop()
