9ce020516ead058cf4caa09e8b748d33a6010225
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2021-04-16 23:00:03 +0100

    Made changes

--- scripts/calculator.py
+++ scripts/calculator.py
@@ -4,13 +4,14 @@ from tkinter import *
 root = Tk()
 root.title("Calculator")
 root.resizable(False, False)
+root.iconbitmap("logos/Calculator_30001.ico")
 root.configure(bg="#fff")
 
 
 # Input field
-e = Entry(root, width=11, justify=RIGHT, bg="#f2f2f0", bd=0, highlightthickness=0, font=("Calibri", 50))
+e = Entry(root, width=12, justify=RIGHT, bg="#f2f2f0", bd=0, highlightthickness=0, font=("Calibri", 50))
 e.config(highlightcolor="#f2f2f0", highlightbackground="#f2f2f0")
-e.grid(row=0, column=0, columnspan=4, ipady=110)
+e.grid(row=0, column=0, columnspan=4, ipady=90)    
 
 # Defining functions
 def buttonClick(num):
@@ -59,9 +60,8 @@ def buttonSqrt():
   global math
   newNum = e.get()
   math = "sqrt"
-  fnum = float(newNum)
+  fnum = newNum
   e.delete(0,END)
-
 
 def buttonPerc():
   global fnum
@@ -128,7 +128,7 @@ def buttonEqual():
 
   if math == "sqrt":
     try:
-        res = round(float(fnum) ** (1/2), 10)
+        res = round(float(snum) ** (1/2), 10)
         if res.is_integer():
             e.insert(0, int(res))
         
@@ -150,77 +150,81 @@ def buttonEqual():
         e.insert(0, "Math Error")
 
 # Defining buttons.
-button_1 = Button(root, text="1", padx=30, pady=17, command=lambda: buttonClick(1), bg="#fff", bd=0, highlightthickness=2,
+button_1 = Button(root, text="1", padx=35, pady=20, command=lambda: buttonClick(1), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_2 = Button(root, text="2", padx=30, pady=17, command=lambda: buttonClick(2), bg="#fff", bd=0, highlightthickness=2,
+button_2 = Button(root, text="2", padx=35, pady=20, command=lambda: buttonClick(2), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_3 = Button(root, text="3", padx=30, pady=17, command=lambda: buttonClick(3), bg="#fff", bd=0, highlightthickness=2,
+button_3 = Button(root, text="3", padx=40, pady=18, command=lambda: buttonClick(3), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
 
-button_4 = Button(root, text="4", padx=30, pady=17, command=lambda: buttonClick(4), bg="#fff", bd=0, highlightthickness=2,
+button_4 = Button(root, text="4", padx=35, pady=20, command=lambda: buttonClick(4), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_5 = Button(root, text="5", padx=30, pady=17, command=lambda: buttonClick(5), bg="#fff", bd=0, highlightthickness=2,
+button_5 = Button(root, text="5", padx=35, pady=20, command=lambda: buttonClick(5), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_6 = Button(root, text="6", padx=30, pady=17, 
+button_6 = Button(root, text="6", padx=35, pady=20, 
 command=lambda: buttonClick(6), bg="#fff", bd=0, highlightthickness=2, font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
 
-button_7 = Button(root, text="7", padx=30, pady=17, command=lambda: buttonClick(7), bg="#fff", bd=0, highlightthickness=2,
+button_7 = Button(root, text="7", padx=35, pady=20, command=lambda: buttonClick(7), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_8 = Button(root, text="8", padx=30, pady=17, command=lambda: buttonClick(8), bg="#fff", bd=0, highlightthickness=2,
+button_8 = Button(root, text="8", padx=35, pady=20, command=lambda: buttonClick(8), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_9 = Button(root, text="9", padx=30, pady=17, command=lambda: buttonClick(9), bg="#fff", bd=0, highlightthickness=2,
+button_9 = Button(root, text="9", padx=35, pady=20, command=lambda: buttonClick(9), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
  
-button_0 = Button(root, text="0", padx=30, pady=17, command=lambda: buttonClick(0), bg="#fff", bd=0, highlightthickness=2,
+button_0 = Button(root, text="0", padx=85, pady=21, command=lambda: buttonClick(0), bg="#fff", bd=0, highlightthickness=2,
                   font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_add = Button(root, text="+", padx=31, pady=17, command=lambda: buttonAdd(), bg="#fff", bd=0, highlightthickness=2,
+button_add = Button(root, text="+", padx=40, pady=20, command=lambda: buttonAdd(), bg="#fff", bd=0, highlightthickness=2,
                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_sub = Button(root, text="-", padx=34, pady=17, command=lambda: buttonSub(), bg="#fff", bd=0, highlightthickness=2,
+button_sub = Button(root, text="-", padx=42.4, pady=20, command=lambda: buttonSub(), bg="#fff", bd=0, highlightthickness=2,
                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
 
-button_mul = Button(root, text="x", padx=33, pady=17, command=lambda: buttonMul(), bg="#fff", bd=0, highlightthickness=2,
+button_mul = Button(root, text="x", padx=41, pady=20, command=lambda: buttonMul(), bg="#fff", bd=0, highlightthickness=2,
                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_div = Button(root, text="÷", padx=30, pady=17, command=lambda: buttonDiv(), bg="#fff", bd=0, highlightthickness=2,
+button_div = Button(root, text="÷", padx=40, pady=20, command=lambda: buttonDiv(), bg="#fff", bd=0, highlightthickness=2,
                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
-button_equal = Button(root, text="=", padx=80, pady=17, command=lambda: buttonEqual(), bg="orange", bd=0, highlightthickness=0,
+button_equal = Button(root, text="=", padx=41.5, pady=23, command=lambda: buttonEqual(), bg="orange", bd=0, highlightthickness=0,
                       font=("Calibri", 18), activebackground="#fff", activeforeground="orange", fg="#fff")
 
-button_clear = Button(root, text="AC", padx=71, pady=14.5, command=lambda: buttonClear(), bg="#fff", bd=0, highlightthickness=2,
+button_clear = Button(root, text="AC", padx=30, pady=17, command=lambda: buttonClear(), bg="#fff", bd=0, highlightthickness=2,
                       font=("Calibri", 18), activebackground="orange", activeforeground="#fff", fg="orange")
-button_sqrt = Button(root, text="√", padx=33, pady=17, command=lambda: buttonSqrt(), bg="#fff", bd=0, highlightthickness=2,
-                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
+button_sqrt = Button(root, text="√", padx=35, pady=17, command=lambda: buttonSqrt(), bg="#fff", bd=0, highlightthickness=2,
+                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff", fg="orange")
 button_perc = Button(root, text="%", padx=34, pady=17, command=lambda: buttonPerc(), bg="#fff", bd=0, highlightthickness=2,
-                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff")
+                     font=("Calibri", 18), activebackground="orange", activeforeground="#fff", fg="orange")
+
+decimal_point = Button(root, text=".", padx=40, pady=13, command=lambda: buttonClick("."), bg="#fff", bd=0, highlightthickness=2,
+                       font=("Calibri", 23), activebackground="orange", activeforeground="#fff")
 
 var_list = [button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9, button_0, button_add, button_sub,
             button_mul, button_div, button_equal, button_clear, button_sqrt, button_perc]
 for i in var_list:
     i.config(highlightbackground="#fff", highlightcolor="#fff")
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
 
-button_0.grid(row=4, column=0)
+button_0.grid(row=5, column=0, columnspan=2)
 
-button_perc.grid(row=4, column=1)
+button_perc.grid(row=1, column=2)
 
-button_sub.grid(row=2, column=3)
-button_add.grid(row=1, column=3)
+button_sub.grid(row=1, column=3)
+button_add.grid(row=2, column=3)
 button_mul.grid(row=3, column=3)
 button_div.grid(row=4, column=3)
-button_sqrt.grid(row=4, column=2)
+button_sqrt.grid(row=1, column=1)
 
-button_clear.grid(row=5, column=0, columnspan=2)
-button_equal.grid(row=5, column=2, columnspan=2)                   
+button_clear.grid(row=1, column=0)
+button_equal.grid(row=5, column=3)                   
+decimal_point.grid(row=5, column=2)
 
 # Event loop
 root.mainloop()
