91548f72bddedcfda4cc2f0974f792cb7a41f7f8
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2021-04-25 16:25:46 +0100

    Made changes

--- scripts/signup.py
+++ scripts/signup.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import random
+import re
 
 
 # Created class to make signup form in tkinter
@@ -10,11 +11,11 @@ class Signup:
         # Root widget
         self.root = root
         self.root.resizable(False, False)
-        self.root.title("Sign up SmartX")
+        self.root.title("Login SmartX")
         self.root.geometry("1000x667")
 
         # Backrgound Image
-        self.bg = ImageTk.PhotoImage(Image.open("AdobeStock_162870060-e1531903647410.jpeg"))
+        self.bg = ImageTk.PhotoImage(Image.open("images/AdobeStock_162870060-e1531903647410.jpeg"))
         self.bg_label = Label(self.root, image=self.bg)
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -23,17 +24,14 @@ class Signup:
         self.frame.place(x=75, y=164, height=340, width=400)
 
         # Title for frame that says Join SmartX
-        self.txt = "Join SmartX"
+        self.txt = "Login Here"
         self.count = 0
-        self.text = ''
-        self.color = ["#4f4e4d","#f29844", "red2", "#00d5ff"]
+        self.text = '' 
+        self.color = ["#00d5ff","#f29844", "red2", "#59ff00"]
         self.heading = Label(self.frame,text=self.txt, font=("Impact", 35),bg="#fff",fg="#00d5ff")
         self.heading.pack(anchor=W)
         self.slider()
-        self.heading_color()
-
-        #self.signup_title = Label(self.frame,text=word[:i], font=("Impact", 35), bg="#fff", fg="#00d5ff")
-        #self.signup_title.pack(anchor=W, pady=(0, 10))		
+        self.heading_color()	
 
         self.email_lbl = Label(self.frame, text="Email", font=("Calibri", 20), bg="#fff", fg="orange")
         self.email_lbl.pack(anchor=W)
@@ -48,8 +46,12 @@ class Signup:
 
         self.entry_p = Entry(self.frame, width=25, font=("Calibri", 15), fg="orange", bg="#fff", highlightthickness=1, bd=0)
         self.entry_p.config(highlightcolor="orange", highlightbackground="orange")
-        self.entry_p.pack(anchor=W)
+        self.entry_p.pack(anchor=W, pady=(0, 40))
 
+        self.signup_btn = Button(self.frame, text="Login", font=("Ubuntu", 20), bg="#00d5ff", fg="#fff", bd=0, padx=40, pady=20)
+        self.signup_btn.pack(anchor=CENTER)
+
+    
     def slider(self):
         if self.count>=len(self.txt):
             self.count = -1
@@ -64,10 +66,12 @@ class Signup:
 
         self.heading.after(100,self.slider)
 
+    
     def heading_color(self):
         fg = random.choice(self.color)
         self.heading.config(fg=fg)
         self.heading.after(50, self.heading_color)
+
 
 
 # Create window
